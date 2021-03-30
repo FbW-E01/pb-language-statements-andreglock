6888660336e0ab6958de9c19755707feea520739
André <andreglock@gmail.com>
2021-03-30 10:05:53 +0200

    done

---
+++ javascript.js
@@ -0,0 +1,10 @@
+console.log("Hello World");
+console.log("This is not my first js file");
+
+/* 
+1. Remove a parenthesis from the first statement and run the code. Does your statement print? 
+No
+2. Is there an error? If so, what kind of error is it? 
+If '(' is removed - SyntaxError: Unexpected string 
+If '(' is removed - SyntaxError: missing ) after argument list
+*/
